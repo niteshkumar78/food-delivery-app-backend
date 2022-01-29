557076f48893d259341fce23462fcbbad32505f9
niteshkumar78 <niteshkumartg@gmail.com>
2022-01-29 18:36:34 +0530

    updated

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const Users = require("./models/user");
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
+app.use(express.static(__dirname + "/public"));
 
 // app.use(cors());
 
